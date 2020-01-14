2a44faae64fe0b7e8576fee38cc45e1ddb8710d7
Rodrigo Jean Santos <rodrigo.j.fcamara@duratex.com.br>
2020-01-14 19:26:35 -0300

    studys

--- 1_2.js
+++ 1_2.js
@@ -1,20 +1,8 @@
 const log = console.log
 
 /* Using normal for loop: */
-function isOdd(n) {
-    return n %2;
- }
  let numbers = [1,2,3,4,5];
- let odd = [];
- for(let i=0, total = numbers.length; i< total ; i= i +1) {
-    let number = numbers[i];
-    if( isOdd(number) ) {
-       odd.push(number);
-    }
- }
-log(odd)
+
 
  /* Using filter */
  let numbersF = [1,2,3,4,5, 6, 7]
-let oddF = numbersF.filter(n => n%2); 
-log(oddF)
--- 1_4.js
+++ 1_4.js
@@ -2,16 +2,7 @@ const log = console.log
 
 //Checking if an Array Contains a Value
 var names = ["ram", "raj", "rahul"];
-for(let i=0, totalNames = names.length; i< totalNames ; i= i +1) {
-   if(names[i] === "rahul") {
-     log("found rahul");
-     // return; 
-   }
-}
+
 
 //Using some
 var namesA = ["ram", "raj", "rahul"];
-let isRamPresent = namesA.some(namesA => namesA==="ram");
-if(isRamPresent) {
-  log("found ram"); 
-}
